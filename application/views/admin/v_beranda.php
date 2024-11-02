2a18c86dbdb1de24f4b365a8ec7baf949ba8cdf1
narutompel <putujackson0@gmail.com>
2024-11-02 23:16:44 +0800

    first commit

---
+++ application/views/admin/v_beranda.php
@@ -0,0 +1,8 @@
+<!-- section intro -->
+    
+      <div class="intro-content">
+        <h2>Selamat Datang <?php echo $this->session->userdata('nama'); ?>Admin</h2>
+        <h3>Pengelolaan Website Sistem Pakar Diagnosa Penyakit Sapi</h3>
+      </div>
+    
+<!-- /section intro -->
